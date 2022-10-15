66916ad1386b4336baf2a7ecbb52d40d8e421f4c
calculator <dnltktsnv2004@gmail.com>
2022-10-15 21:03:48 +0300

    commit1

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -5,10 +5,26 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        ApplicationContext applicationContext1 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext1.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Cat cat1 = context.getBean("cat", Cat.class);
+        Cat cat2 = context.getBean("cat", Cat.class);
+
+        System.out.println(applicationContext == applicationContext1);
+        System.out.println(cat1 == cat2);
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -1,14 +1,24 @@
 package peaksoft;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getBeanCat() {
+        return new Cat();
     }
 }
